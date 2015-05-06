b6b4cb3e100c770e27993a0514d0131586b67abc
Vincent <vincentlkl@gmail.com>
2015-05-06 14:46:09 +0800

    added devise invitable gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,68 +2,68 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :invitable, :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :invitable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :admin, :staff, :author, :first_name, :last_name, :publisher_id, :library_id, :username, :publisher, :library
   # attr_accessible :title, :body
-  
+
   belongs_to :publisher
   belongs_to :library
   has_many :authors, dependent: :destroy
   has_many :responses, :dependent => :destroy
   has_many :publications, :dependent => :destroy
-  
+
   SUPER_USER_TYPES = ["", "Superadmin", "Admin", "Staff", "Author"]
   USER_TYPES = ["", "Admin", "Staff", "Author"]
-  
+
   def to_s
     self.full_name
   end
-  
+
   def can_delete?
     a = true
     self.responses.each do |resp|
       if !resp.lib_exported_flag || !resp.pub_exported_flag
         a = false
         break
-      end    
+      end
     end
     return a
   end
-  
+
   def full_name
     return "#{self.first_name} #{self.last_name}"
   end
-  
+
   def full_name_email
     return "#{self.last_name}, #{self.first_name} (#{self.email})"
   end
-  
+
   def self.random_password(size = 11)
     chars = (('a'..'z').to_a + ('0'..'9').to_a) - %w(i o 0 1 l 0)
     (1..size).collect{|a| chars[rand(chars.size)] }.join
   end
-  
+
   def is_librarian?
     if self.is_lib_admin? || self.is_lib_staff?
       return true
-    end  
-  end  
-  
+    end
+  end
+
   def is_publisher?
     if self.is_pub_admin? || self.is_pub_staff?
       return true
-    end  
+    end
   end
-  
+
   def is_author?
     if self.author
       return true
-    end  
+    end
   end
-  
+
   def user_type
     if self.author
       return "Author"
@@ -74,26 +74,26 @@ class User < ActiveRecord::Base
     elsif self.superadmin
       return "Superadmin"
     else
-      return "Unassigned"  
-    end 
-  end  
-  
+      return "Unassigned"
+    end
+  end
+
   def is_lib_admin?
     self.try(:admin?) && !self.library.nil?
-  end  
-  
+  end
+
   def is_lib_staff?
     self.try(:staff?) && !self.library.nil?
-  end 
-  
+  end
+
   def is_pub_admin?
     self.try(:admin?) && !self.publisher.nil?
-  end 
-  
+  end
+
   def is_pub_staff?
     self.try(:staff?) && !self.publisher.nil?
-  end 
-  
+  end
+
   def my_authors
     authors = Array.new
     if self.superadmin
@@ -101,11 +101,11 @@ class User < ActiveRecord::Base
     elsif self.is_pub_admin? || self.is_pub_staff?
       authors = User.all(:conditions => {:publisher_id => self.publisher.id, :author => true})
     elsif self.is_lib_admin? || self.is_lib_staff?
-      authors = User.all(:conditions => {:library_id => self.library.id, :author => true})  
+      authors = User.all(:conditions => {:library_id => self.library.id, :author => true})
     end
     return authors
   end
-  
+
   def my_staff
     staff = Array.new
     if self.superadmin
@@ -113,11 +113,11 @@ class User < ActiveRecord::Base
     elsif self.is_pub_admin? || self.is_pub_staff?
       staff = User.all(:conditions => {:publisher_id => self.publisher.id, :staff => true})
     elsif self.is_lib_admin? || self.is_lib_staff?
-      staff = User.all(:conditions => {:library_id => self.library.id, :staff => true})   
+      staff = User.all(:conditions => {:library_id => self.library.id, :staff => true})
     end
     return staff
   end
-  
+
   def my_admin
     admin = Array.new
     if self.superadmin
@@ -125,11 +125,11 @@ class User < ActiveRecord::Base
     elsif self.is_pub_admin? || self.is_pub_staff?
       admin = User.all(:conditions => {:publisher_id => self.publisher.id, :admin => true})
     elsif self.is_lib_admin? || self.is_lib_staff?
-      admin = User.all(:conditions => {:library_id => self.library.id, :admin => true})   
+      admin = User.all(:conditions => {:library_id => self.library.id, :admin => true})
     end
     return admin
   end
-  
+
   def my_unassigned
     unassigned = Array.new
     if self.superadmin
@@ -137,11 +137,11 @@ class User < ActiveRecord::Base
     elsif self.is_pub_admin? || self.is_pub_staff?
       unassigned = User.all(:conditions => ["admin is not true and staff is not true and author is not true and publisher_id = ?", self.publisher.id])
     elsif self.is_lib_admin? || self.is_lib_staff?
-      unassigned = User.all(:conditions => ["admin is not true and staff is not true and author is not true and library_id = ?", self.library.id])   
+      unassigned = User.all(:conditions => ["admin is not true and staff is not true and author is not true and library_id = ?", self.library.id])
     end
     return unassigned
   end
-  
+
   def affiliation
     if !self.publisher.nil?
       return self.publisher.name
@@ -149,13 +149,13 @@ class User < ActiveRecord::Base
       return self.library.name
     else
       return "Unaffiliated"
-    end      
+    end
   end
-  
+
   def find_profile(publisher)
     Author.first(:conditions => {:user_id => self.id, :publisher_id => publisher})
-  end  
-  
+  end
+
   def send_new_publisher_user_email(email, path)
     # send to selected users
     Email.create(
@@ -164,9 +164,9 @@ class User < ActiveRecord::Base
       :to => email,
       :subject => "[Author Names] Please Sign Up",
       :body => "<p>Please <a href='#{ROOT_URL}#{path}'>create</a> an account.</p>"
-    )   
-  end 
-  
+    )
+  end
+
   def send_new_library_user_email(email, path)
     # send to selected users
     Email.create(
@@ -175,6 +175,6 @@ class User < ActiveRecord::Base
       :to => email,
       :subject => "[Author Names] Please Sign Up",
       :body => "<p>Please <a href='#{ROOT_URL}#{path}'>create</a> an account.</p>"
-    )   
-  end 
+    )
+  end
 end
