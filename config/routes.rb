9e2253670cb50be68c488f1aca0293d08cf3c408
Vincent <vincentlkl@gmail.com>
2015-05-11 10:37:15 +0800

    added institution model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  resources "institutions"
+
   devise_for :users
   root 'pages#home'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150511023143) do
+ActiveRecord::Schema.define(version: 20150511023354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "institutions", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "contact_name"
+    t.string   "phone"
+    t.string   "email"
+    t.string   "address_1"
+    t.string   "address_2"
+    t.string   "city"
+    t.string   "state"
+    t.string   "postal_code"
+    t.string   "country"
+    t.string   "website"
+    t.string   "logo"
+    t.string   "company_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
