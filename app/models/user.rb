a24e5910f5f656713d1627774bd11d6c40166416
Vincent <vincentlkl@gmail.com>
2015-05-11 09:40:52 +0800

    initial commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,17 +30,16 @@
 #
 
 class User < ActiveRecord::Base
-  enum role: [:superadmin, :admin, :user]
-  after_initialize :set_default_role, :if => :new_record?
-
-  def set_default_role
-    self.role ||= :user
-  end
+  # enum role: [:superadmin, :admin, :user]
+  # after_initialize :set_default_role, :if => :new_record?
+  #
+  # def set_default_role
+  #   self.role ||= :user
+  # end
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :invitable
+         :recoverable, :rememberable, :trackable, :validatable, :invitable
 
 end
