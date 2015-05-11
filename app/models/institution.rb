e1006abcbe2b823239892353ff83eb2093b30f9a
Vincent <vincentlkl@gmail.com>
2015-05-11 10:39:03 +0800

    added strong parameters to institution

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -1,2 +1,10 @@
 class Institution < ActiveRecord::Base
+
+  def self.strong_parameters
+    columns =[:id, :name, :description,
+    :contact_name, :phone, :email, :address_1, :address_2,
+    :city, :state, :postal_code, :country, :website,
+    :logo, :company_type]
+    columns
+  end
 end
