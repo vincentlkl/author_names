8a8970e016d7efd815f4f0bd80d5a8e67cf86f9b
Vincent <vincentlkl@gmail.com>
2015-05-12 13:47:37 +0800

    change role for institution role

--- app/models/institution_role.rb
+++ app/models/institution_role.rb
@@ -1,9 +1,9 @@
 class InstitutionRole < ActiveRecord::Base
-  enum role: [:admin, :author, :publisher]
+  enum role: [:admin, :staff, :author]
   after_initialize :set_default_role, :if => :new_record?
 
   def set_default_role
-    self.role ||= :admin
+    self.role ||= :author
   end
 
   belongs_to :institution
